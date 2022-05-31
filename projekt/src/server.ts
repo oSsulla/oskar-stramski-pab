669c32adeea63e5788a8bc1a0b2d51a74b6b49e3
oSsulla <osa2000osa@gmail.com>
2022-05-31 15:17:30 +0200

    .

---
+++ projekt/src/server.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+import http from 'http';
+import mongoose from 'mongoose';
+import { config } from './config/config';
+
+const router = express();
+
+// Łączenie z Mongo //
+mongoose.connect(config.mongo.url, { retryWrites: true, w: 'majority' })
+.then(() => { 
+    console.log('connected');
+})
+.catch(error => {
+    console.log(error);
+})
